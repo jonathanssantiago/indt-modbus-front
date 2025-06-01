65464bf87d8a97c31f2ef8aa03e9b527d6c3b373
Jonathan Santiago <jonathan.santiago@creathus.org.br>
2025-05-31 21:16:39 -0400

    ci: add deployment workflow to AWS S3 and CloudFront, updates Next.js config, and enhances build commands

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   poweredByHeader: false,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix:
+    process.env.NODE_ENV === 'production'
+      ? process.env.NEXT_PUBLIC_CDN_URL
+      : '',
 };
 
 export default nextConfig;
